01539fa9724721449ccc68debc2d99a7981cdde4
Vicky <6hz@6hzdeMacBook-Pro.local>
2022-06-11 23:13:39 +0800

    FEAT: add webpack

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,160 +11,163 @@ module.exports = {
     themeConfig: {
         sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
         // lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-        nav:[
+        nav: [
             { text: 'Blog', link: '/blog/basic/mind' }, // 内部链接 以docs为根目录
             // { text: 'Photos',  link: '/photos/guild'}, // 内部链接 以docs为根目录
             // { text: 'Debug',link: '/debug/css'}, 
-            { text: '八股文',link: '/question/guild'},
-            { text: 'Debug',link: '/debug/guild'},
-            { text: 'Tool', link: '/tools/markdown-editor'},
-            { text: 'Libraly', 
-              items: [
-                { text: '腾讯前端团队', link:'http://www.alloyteam.com/page/0/'},
-                { text: '淘宝前端团队', link:'https://fed.taobao.org/'},
-                { text: 'Emoji', link:'https://www.webfx.com/tools/emoji-cheat-sheet/'},
-                { text: '在线工具', link:'https://tool.oschina.net/commons'},
-                { text: '设计导航', link:'http://hao.shejidaren.com/'},
-                { text: 'Typing', link:'https://www.keybr.com/'},
+            { text: '八股文', link: '/question/guild' },
+            { text: 'Debug', link: '/debug/guild' },
+            { text: 'Tool', link: '/tools/markdown-editor' },
+            {
+                text: 'Libraly',
+                items: [
+                    { text: '腾讯前端团队', link: 'http://www.alloyteam.com/page/0/' },
+                    { text: '淘宝前端团队', link: 'https://fed.taobao.org/' },
+                    { text: 'Emoji', link: 'https://www.webfx.com/tools/emoji-cheat-sheet/' },
+                    { text: '在线工具', link: 'https://tool.oschina.net/commons' },
+                    { text: '设计导航', link: 'http://hao.shejidaren.com/' },
+                    { text: 'Typing', link: 'https://www.keybr.com/' },
                 ]// 外部链接
-            },            
+            },
             // 下拉列表
-            { text: 'About Me',
-              items: [
-                  { text: 'GitHub地址', link: 'https://github.com/banana6hz' },
-              ]
-            }        
+            {
+                text: 'About Me',
+                items: [
+                    { text: 'GitHub地址', link: 'https://github.com/banana6hz' },
+                ]
+            }
         ],
         sidebar: {
             // 侧边栏在 /blog/上
             '/blog/': [
-                ['/blog/basic/mind','我的前端'],
+                ['/blog/basic/mind', '我的前端'],
                 {
-                    title:'CSS',
+                    title: 'CSS',
                     collapsable: true,
                     children: [
-                        ['/blog/basic/css/guild',':pointer:css基础'],
-                        ['/blog/basic/css/flex','flex布局'],
-                        ['/blog/basic/css/z-index','深入理解z-index']
+                        ['/blog/basic/css/guild', ':pointer:css基础'],
+                        ['/blog/basic/css/flex', 'flex布局'],
+                        ['/blog/basic/css/z-index', '深入理解z-index']
                     ]
                 },
                 {
-                    title:'JavaScript',
+                    title: 'JavaScript',
                     collapsable: true,
                     children: [
-                        ['/blog/basic/javascript/guild','JS基础'],
-                        ['/blog/basic/javascript/caozuofu','操作符'],
-                        ['/blog/basic/javascript/event','事件'],
-                        ['/blog/basic/javascript/eventloop','JS循环机制'],
-                        ['/blog/basic/javascript/es6','ES6']
+                        ['/blog/basic/javascript/guild', 'JS基础'],
+                        ['/blog/basic/javascript/caozuofu', '操作符'],
+                        ['/blog/basic/javascript/event', '事件'],
+                        ['/blog/basic/javascript/eventloop', 'JS循环机制'],
+                        ['/blog/basic/javascript/es6', 'ES6']
                     ]
                 },
                 {
-                    title:'ES6',
+                    title: 'ES6',
                     collapsable: true,
                     children: [
-                        ['/blog/es6/promise','Promise']
+                        ['/blog/es6/promise', 'Promise']
                     ]
                 },
                 {
-                    title:'TypeScript',
+                    title: 'TypeScript',
                     collapsable: true,
                     children: [
                         ['/blog/basic/typescript/guild', 'TS基础']
                     ]
                 },
                 {
-                    title:'网络/浏览器',
+                    title: '网络/浏览器',
                     collapsable: true,
                     children: [
-                        ['/blog/basic/net/guild','网络'],
-                        ['/blog/basic/net/http','HTTP协议'],
-                        ['/blog/basic/net/tcp','TCP/IP协议'],
-                        ['/blog/basic/net/session_cookie','cookie与session'],
-                        ['/blog/basic/net/browserRendering','浏览器渲染'],
-                        ['/blog/basic/net/huancun','浏览器缓存']
+                        ['/blog/basic/net/guild', '网络'],
+                        ['/blog/basic/net/http', 'HTTP协议'],
+                        ['/blog/basic/net/tcp', 'TCP/IP协议'],
+                        ['/blog/basic/net/session_cookie', 'cookie与session'],
+                        ['/blog/basic/net/browserRendering', '浏览器渲染'],
+                        ['/blog/basic/net/huancun', '浏览器缓存']
                     ]
                 },
-                ['/blog/jquery/guild','jQuery'],
+                ['/blog/jquery/guild', 'jQuery'],
                 {
-                    title:'VUE',
+                    title: 'VUE',
                     collapsable: true,
                     children: [
-                        ['/blog/vue/guild','Vue指南'],
-                        ['/blog/vue/myvue','vue手动搭建'],
-                        ['/blog/vue/vuelife','vue生命周期'],
-                        ['/blog/vue/zujiantongxin','Vue组件的通信'],
-                        ['/blog/vue/mock','mock假数据'],
-                        ['/blog/vue/build','vue的工程化'],
-                        ['/blog/vue/beforeEach','动态路由'],
+                        ['/blog/vue/guild', 'Vue指南'],
+                        ['/blog/vue/myvue', 'vue手动搭建'],
+                        ['/blog/vue/vuelife', 'vue生命周期'],
+                        ['/blog/vue/zujiantongxin', 'Vue组件的通信'],
+                        ['/blog/vue/mock', 'mock假数据'],
+                        ['/blog/vue/build', 'vue的工程化'],
+                        ['/blog/vue/beforeEach', '动态路由'],
                     ]
                 },
                 {
-                    title:'微信小程序',
+                    title: '微信小程序',
                     collapsable: true,
                     children: [
-                        ['/blog/wxapp/guild','微信小程序'],
-                        ['/blog/wxapp/wxapp','微信小程序开发流程']
+                        ['/blog/wxapp/guild', '微信小程序'],
+                        ['/blog/wxapp/wxapp', '微信小程序开发流程']
                     ]
                 },
                 {
-                    title:'Node.js',
+                    title: 'Node.js',
                     collapsable: true,
                     children: [
-                        ['/blog/node/guild','Node.js'],
-                        ['/blog/koa/koaGuild','koa指南'],
+                        ['/blog/node/guild', 'Node.js'],
+                        ['/blog/koa/koaGuild', 'koa指南'],
                     ]
                 },
                 {
-                    title:'工具',
+                    title: '工具',
                     collapsable: true,
                     children: [
-                        ['/blog/tool/git/guild','Git指南'],
-                        ['/blog/tool/vuepress/guild','vuepress指南'],
-                        ['/blog/tool/markdown/guild','markdown指南'],
-                        ['/blog/tool/filezilla/guild','FileZilla指南'],
-                        ['/blog/tool/mock/mock','mock.js指南'],
-                        ['/blog/tool/webpack/guild','webpack指南']
+                        ['/blog/tool/git/guild', 'Git指南'],
+                        ['/blog/tool/vuepress/guild', 'vuepress指南'],
+                        ['/blog/tool/markdown/guild', 'markdown指南'],
+                        ['/blog/tool/filezilla/guild', 'FileZilla指南'],
+                        ['/blog/tool/mock/mock', 'mock.js指南'],
+                        ['/blog/tool/webpack/guild', 'webpack指南']
                     ]
-                // {
-                //     title:'实现',
-                //     collapsable: true,
-                //     children: [
-                //         ['/blog/zaza/center','水平垂直居中'],
-                //     ]
-                // }
+                    // {
+                    //     title:'实现',
+                    //     collapsable: true,
+                    //     children: [
+                    //         ['/blog/zaza/center','水平垂直居中'],
+                    //     ]
+                    // }
                 },
                 {
-                    title:'专题',
-                    children:[
-                        ['/blog/summary/xiangyingshi','响应式布局'],
-                        ['/blog/summary/shuzu','数组操作合集'],
-                        ['/blog/summary/zifuchuan','字符串操作合集'],
+                    title: '专题',
+                    children: [
+                        ['/blog/summary/xiangyingshi', '响应式布局'],
+                        ['/blog/summary/shuzu', '数组操作合集'],
+                        ['/blog/summary/zifuchuan', '字符串操作合集'],
                     ]
                 },
                 {
-                    title:'算法',
-                    collapsable:true,
-                    children:[
-                        ['/blog/suanfa/guild','算法'],
+                    title: '算法',
+                    collapsable: true,
+                    children: [
+                        ['/blog/suanfa/guild', '算法'],
                     ]
                 }
             ],
             '/question/': [
-                ['/question/guild','八股文'],
-                ['/question/html/basic','HTML'],
-                ['/question/css/css01', 'CSS'],  
-                ['/question/js/js01','JavaScript'],
-                ['/question/es6/es6_01','ES6'],
-                ['/question/vue/vue01','Vue'],
-                ['/question/jquery/jq01','jquery'],
-                ['/question/js/js02','下面代码输出什么？'],
+                ['/question/guild', '八股文'],
+                ['/question/html/basic', 'HTML'],
+                ['/question/css/css01', 'CSS'],
+                ['/question/js/js01', 'JavaScript'],
+                ['/question/es6/es6_01', 'ES6'],
+                ['/question/vue/vue01', 'Vue'],
+                ['/question/jquery/jq01', 'jquery'],
+                ['/question/webpack/webpack01', 'webpack'],
+                ['/question/js/js02', '下面代码输出什么？'],
                 {
-                    title:'计算机网络',
+                    title: '计算机网络',
                     collapsable: true,
                     children: [
-                        ['/question/net/url','输入URL到页面渲染出来，浏览器经历了什么？'],
-                        ['/question/net/kuayu','浏览器跨域'],
+                        ['/question/net/url', '输入URL到页面渲染出来，浏览器经历了什么？'],
+                        ['/question/net/kuayu', '浏览器跨域'],
                     ]
                 }
             ],
@@ -180,21 +183,21 @@ module.exports = {
             ],
             // 侧边栏在 /debug/ 上
             '/debug/': [
-                ['/debug/js','JS'],
+                ['/debug/js', 'JS'],
                 ['/debug/css', 'CSS'],
                 ['/debug/net', 'NET']
             ],
             // 侧边栏在 /photos/ 上
             '/photos/': [
                 {
-                    title:'view',
+                    title: 'view',
                     collapsable: true,
                     children: [
-                        ['/photos/view','view']
+                        ['/photos/view', 'view']
                     ]
                 },
                 {
-                    title:'eat',
+                    title: 'eat',
                     collapsable: true,
                     children: [
                         ['/photos/eat', 'eat']
--- docs/question/webpack/webpack01.md
+++ docs/question/webpack/webpack01.md
@@ -1,3 +1,45 @@
+## :white_medium_square: 1. Webpack 配置中用过哪些 Loader ？都有什么作用？
+首先我们要知道为什么需要loader，这是因为webpack默认只能处理js和json文件，当遇到一些比如css这样的文件时，需要先将这些文件进行转化成webpack能识别的文件，这就需要loader来对不同类型的文件进行处理了。
+webpack 支持 loader 的链式调用，即一个文件可以经多个 loader 处理。当一个文件使用多个 loader 处理时，他的处理顺序是倒序的，即传入 loader 数组的从右到左执行。
+- css-loader ：负责解析css代码, 处理css中的依赖，将多个css文件整合到一起
+- style-loader ：将处理好的 css 通过 style 标签的形式添加到页面上。(核心逻辑：通过动态添加 style 标签的方式，将样式引入页面)
+- postcss-loader：自动添加 CSS3 部分属性的浏览器前缀（eg:transform: translateX(-50%);，需要加上不同的浏览器前缀)
+- less-loader:  处理less文件
+- sass-loader：处理sass文件，注意还需要搭配 node-sass
+- file-loader：解决图片引入问题，比如background样式用url引入背景图，file-loader可以解析项目中的url引入（不仅限于css），根据我们的配置，将图片拷贝到相应的路径，默认为 dist，再根据我们的配置，修改打包后文件引用路径，使之指向正确的文件
+- url-loader：依赖 file-loader，当图片/文字文件小于 limit 值的时候，会将图片转为 base64 编码，大于 limit 值的时候依然是使用 file-loader 进行拷贝（在 webpack5，内置了资源处理模块，file-loader 和 url-loader 都可以不用安装）
+- img-loader：压缩图片
+- babel-loader： 将ES6转换为了 ES5， 为了避免 webpack.config.js 太臃肿，babeld的配置一般会单独提取到一个文件，在babel的配置文件里面也可以配置一些插件
+>在这里补充下babel的其他知识
+- @babel/core Babel 编译的核心包
+- @babel/preset-env Babel 编译的预设，可以理解为 Babel 插件的超集
+```js
+// ./babelrc.js
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        // useBuiltIns: false 默认值，无视浏览器兼容配置，引入所有 polyfill
+        // useBuiltIns: entry 根据配置的浏览器兼容，引入浏览器不兼容的 polyfill
+        // useBuiltIns: usage 会根据配置的浏览器兼容，以及你代码中用到的 API 来进行 polyfill，实现了按需添加
+        useBuiltIns: "entry",
+        corejs: "3.9.1", // 是 core-js 版本号
+        targets: {
+          chrome: "58",
+          ie: "11",
+        },
+      },
+    ],
+  ],
+};
+```
+
+>对于正在提案中，还未进入 ECMA 规范中的新特性，Babel 是无法进行处理的，必须要安装对应的插件，并加上插件的配置
+- vue-loader: 主要用来处理vue组件文件
+- eslint-loade: 将EsLint集成到webpack中
+- cache-loader：缓存一些性能开销比较大的 loader 的处理结果，提高构建速度
+
 ## 什么是bundle、chunk、module
 ## webpack的构建流程
 ## webpack各个loader的作用
